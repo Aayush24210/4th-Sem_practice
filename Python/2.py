266525d092ee606c5ac3b7c99c43397593615436
Aayush24210 <acchourasiya2007@gmail.com>
2026-04-09 22:37:01 +0530

    initial commit

---
+++ Python/2.py
@@ -0,0 +1,13 @@
+# #write a function to evaluate which functionn is evevn factorial and which one is odd factrial
+# def factors(n):
+#     for n == 0 or n == 1
+#         return 1
+# else 
+
+
+list = [1,2,3,"Hello",5.7]
+print(list)
+for i in range(len(list)):
+    print(list[i],end=" ")
+
+    
