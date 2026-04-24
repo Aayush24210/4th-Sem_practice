a34da9faa628eea39d8d068bf23cee4db2bed659
Aayush24210 <acchourasiya2007@gmail.com>
2026-04-24 10:34:10 +0530

    modify

--- Python/first.py
+++ Python/first.py
@@ -1,3 +1,44 @@
-# write a code to print list comprehension of  sum of the given line
-list = [[1,2,3],[3,4,5],[5,7,3,2]]
+# # apppend in list
+# # lst1 = [[1,2,3],[3,4,5],[5,7,3,2]]
+# # lst1.append(5)
+# # print(lst1)
 
+# # insert in list
+# # lst2 = [[1,2,3,4,5],[3,4,6,7],[5,4,2,1]]
+# # lst2.insert(3,[3,4,6,7,8])
+# # print(lst2)
+
+# tpl= "dfgh"
+# print(type(tpl))
+# # 
+mystring = "python Thu`thorials"
+print(mystring[-3:8:-1])
+# print(mystring[-2:4:-1])
+x= "NOOB🫩"
+# i=0
+# for i in 100:
+#     print(x)
+#     break
+
+# # strs
+# .capatlize 
+# .split(sep=",")
+# .Find
+# .replace
+
+
+# # Split 
+# str = "Hello World i am learning pthon with Thu`torial"
+# lst  = str.split(sep="`")
+# print(lst)
+
+# # aayush 
+# word_list = ["hello ,user,i"]
+# temp=" ",
+# print(temp)
+
+
+# str.change 
+# str=("hello , world , user,  aim")
+# # str.replace="," with "."
+# for i in 
