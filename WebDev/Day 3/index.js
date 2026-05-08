9dcf4e0a44a91e2d79044d05458538d18e9639da
Aayush24210 <acchourasiya2007@gmail.com>
2026-05-08 11:16:13 +0530

    inital commit

---
+++ WebDev/Day 3/index.js
@@ -0,0 +1,63 @@
+Sum();
+function Sum() {
+    console.log("Hello");
+} // Hello   // In this you can call the function before & after the function declaration because of hoisting.
+
+
+
+
+
+// But in arrow function you cannot call the function before the declaration because it is not hoisted.
+sum();
+const sum = () => {
+    console.log("Hello");
+} // Uncaught ReferenceError: Cannot access 'sum' before initialization
+
+
+
+console.log(a);
+var a = 5; // undefined   // In this you can access the variable before the declaration because of hoisting. 
+
+
+console.log(b);
+let b = 10; // Uncaught ReferenceError: Cannot access 'b' before initialization
+
+
+
+let arr = [1, 2, 3, 4, 5];
+console.log(arr.length); // 5
+arr.push(6);
+console.log(arr); // [1, 2, 3, 4, 5, 6]
+arr.pop();
+console.log(arr,"pop"); // [1, 2, 3, 4, 5]
+arr.unshift(0);
+console.log(arr,"unshift"); // [0, 1, 2, 3, 4, 5]
+arr.shift();
+console.log(arr,"shift"); // [1, 2, 3, 4, 5]
+console.log(arr,indexOf(3)); // 2
+console.log(arr.includes(11),"Kyaaak");
+console.log();
+
+let data  = arr.find((function (element) {
+    return element === 4;
+}));
+console.log(data,"find"); // 4
+
+arr.forEach((a, b, c) => {
+    console.log(c,forEach);
+}); // [1, 2, 3, 4, 5] forEach
+
+
+
+arr.map((a) => {
+    return a * 2;
+}
+); // [2, 4, 6, 8, 10] map
+arr.filter((a) => {
+    return a % 2 === 0;
+}
+); // [2, 4] filter
+
+arr.forEach((a, b, c) => {
+    console.log(c,forEach);
+}); // [1, 2, 3, 4, 5] forEach
